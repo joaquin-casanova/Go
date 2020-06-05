85ade2e750492515c6bfb68202d343e0e5b27e32
Joaquin Casanova Cordova <joaquin.casanova.c@gmail.com>
2020-06-05 18:46:10 -0500

    how to write commentatios and documentation

---
+++ comentarios/main.go
@@ -0,0 +1,14 @@
+// package main es el paquete principal
+// que me permite ejecutar mi aplicación
+package main
+
+// Test es una variables de prueba
+var Test = 123
+
+// Pi es una constante
+const Pi = 3.14
+
+/* func main()  {
+
+}
+*/
